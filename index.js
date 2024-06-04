d4b687921916b69c8a5fa4459cb01fcfb6b48a3a
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-06-04 22:51:22 +0600

    cors error and env update

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    credentials: true,
+  })
+);
 
 // .env
 // DB_USER=samirunshuvo
